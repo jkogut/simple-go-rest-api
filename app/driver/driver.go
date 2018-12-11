d2f475485e4bda5c41dbcf2ad8828299d00beda1
Jan Kogut <mr.jkogut@gmail.com>
2018-12-11 23:25:35 +0100

    ADD: driver for postgresDB

---
+++ app/driver/driver.go
@@ -0,0 +1,39 @@
+package driver
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
+)
+
+var db *sql.DB
+
+// "LogFatal" func for logging errors
+func LogFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pgInit() {
+	gotenv.Load()
+}
+
+// ConnectDB: func for db connection init
+func ConnectDB() *sql.DB {
+	pgInit()
+	pgURL, err := pq.ParseURL(os.Getenv("PG_URL"))
+	log.Println(pgURL)
+	LogFatal(err)
+
+	db, err = sql.Open("postgres", pgURL)
+	LogFatal(err)
+
+	err = db.Ping()
+	LogFatal(err)
+
+	return db
+}
