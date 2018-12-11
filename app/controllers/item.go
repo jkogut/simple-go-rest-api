b70fb51526105ad2acdad60b932c27b6ed490665
Jan Kogut <mr.jkogut@gmail.com>
2018-12-11 23:24:09 +0100

    ADD: item controllers structure

---
+++ app/controllers/item.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"app/driver"
+	"app/models"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Controller: data struct for handle functions
+type Controller struct{}
+
+var books []models.Book
+
+func apiLogger(rq *http.Request) {
+	log.Println(rq.RemoteAddr, rq.RequestURI, rq.Method)
+}
+
+// Controller.GetItems: handle func for GET method (get all items)
+func (c Controller) GetItems(db *sql.DB) http.HandlerFunc {
+}
+
+// Controller.GetItem: handle func for GET method (get single item)
+func (c Controller) GetItem(db *sql.DB) http.HandlerFunc {
+}
+
+// Controller.AddItem: handle func for POST method (add single item)
+func (c Controller) AddItem(db *sql.DB) http.HandlerFunc {
+}
+
+// Controller.UpdateItem: handle func for PUT method (update single item)
+func (c Controller) UpdateItem(db *sql.DB) http.HandlerFunc {
+}
+
+//Controller.RemoveItem: handle func for DELETE method (delete single item)
+func (c Controller) RemoveItem(db *sql.DB) http.HandlerFunc {
+}
