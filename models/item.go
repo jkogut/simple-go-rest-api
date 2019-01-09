7fada401d79a879be4c8fa6f80d9ca721c2a4e78
Jan Kogut <mr.jkogut@gmail.com>
2019-01-09 23:31:23 +0100

    UPDATE: struct vars rename

--- models/item.go
+++ models/item.go
@@ -1,8 +1,8 @@
 package models
 
 type Item struct {
-	ID     int     `json:"id"`
-	Name1  string  `json:"name1"`
-	Name2  string  `json:"name2"`
-	Name3  string  `json:"name3"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
 }
