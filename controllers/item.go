75248c5511cbfc20268267fb48f78a35444e2a0e
Jan Kogut <mr.jkogut@gmail.com>
2019-01-09 23:44:11 +0100

    UPDATE: struct vars

--- controllers/item.go
+++ controllers/item.go
@@ -31,7 +31,7 @@ func (c Controller) GetBooks(db *sql.DB) http.HandlerFunc {
 		defer rows.Close()
 
 		for rows.Next() {
-			err := rows.Scan(&book.ID, &book.Name1, &book.Name2, &book.Name3)
+			err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
 			driver.LogFatal(err)
 			books = append(books, book)
 		}
@@ -47,7 +47,7 @@ func (c Controller) GetBook(db *sql.DB) http.HandlerFunc {
 		params := mux.Vars(rq)
 
 		row := db.QueryRow("select * from books where id=$1", params["id"])
-		err := row.Scan(&book.ID, &book.Name1, &book.Name2, &book.Name3)
+		err := row.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
 		driver.LogFatal(err)
 
 		// defer row.Close() ???
@@ -64,7 +64,7 @@ func (c Controller) AddBook(db *sql.DB) http.HandlerFunc {
 
 		json.NewDecoder(rq.Body).Decode(&book)
 		//log.Println(book)
-		err := db.QueryRow("insert into books (name1, name2, name3) values ($1, $2, $3) RETURNING id", book.Name1, book.Name2, book.Name3).Scan(&bookID)
+		err := db.QueryRow("insert into books (title, author, year) values ($1, $2, $3) RETURNING id", book.Title, book.Author, book.Year).Scan(&bookID)
 		driver.LogFatal(err)
 		json.NewEncoder(w).Encode(bookID)
 		apiLogger(rq)
@@ -78,7 +78,7 @@ func (c Controller) UpdateBook(db *sql.DB) http.HandlerFunc {
 
 		json.NewDecoder(rq.Body).Decode(&book)
 
-		result, err := db.Exec("update books set name1=$1, name2=$2, name3=$3 where id=$4 RETURNING id", &book.Name1, &book.Name2, &book.Name3, &book.ID)
+		result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 RETURNING id", &book.Title, &book.Author, &book.Year, &book.ID)
 		driver.LogFatal(err)
 
 		rowsUpdated, err := result.RowsAffected()
