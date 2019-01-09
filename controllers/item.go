9d8e55af10cc2451f104e99e2c69c29bc1a8e86a
Jan Kogut <mr.jkogut@gmail.com>
2019-01-09 23:22:54 +0100

    UPDATE: go-lin satisfied with proper exported func comments

--- controllers/item.go
+++ controllers/item.go
@@ -20,7 +20,7 @@ func apiLogger(rq *http.Request) {
 	log.Println(rq.RemoteAddr, rq.RequestURI, rq.Method)
 }
 
-// Controller.GetItems : handle func for GET method (get all items)
+// GetItems : handle func for GET method (get all items)
 func (c Controller) GetItems(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, rq *http.Request) {
 		var item models.Item
@@ -40,7 +40,7 @@ func (c Controller) GetItems(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-// Controller.GetItem : handle func for GET method (get single item)
+// GetItem : handle func for GET method (get single item)
 func (c Controller) GetItem(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, rq *http.Request) {
 		var item models.Item
@@ -56,7 +56,7 @@ func (c Controller) GetItem(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-// Controller.AddItem : handle func for POST method (add single item)
+// AddItem : handle func for POST method (add single item)
 func (c Controller) AddItem(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, rq *http.Request) {
 		var item models.Item
@@ -71,7 +71,7 @@ func (c Controller) AddItem(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-// Controller.UpdateItem : handle func for PUT method (update single item)
+// UpdateItem : handle func for PUT method (update single item)
 func (c Controller) UpdateItem(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, rq *http.Request) {
 		var item models.Item
@@ -89,7 +89,7 @@ func (c Controller) UpdateItem(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-//Controller.RemoveItem : handle func for DELETE method (delete single item)
+// RemoveItem : handle func for DELETE method (delete single item)
 func (c Controller) RemoveItem(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, rq *http.Request) {
 		params := mux.Vars(rq)
